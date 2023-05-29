861b7cd6573e6dfb9e0ebcee14a9bb785092ebef
wuhao <1132276876@qq.com>
2023-05-29 09:16:19 +0800

    0.0

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use actix_web::middleware::Logger;
 use actix_web::{
     error, get, post,
     web::{self, Json, ServiceConfig},
-    Result,
+    App, HttpServer, Responder, Result,
 };
 use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
-use sqlx::{Executor, FromRow, PgPool};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Executor, FromRow, PgPool, Pool, Postgres};
 
 #[get("/{id}")]
 async fn retrieve(path: web::Path<i32>, state: web::Data<AppState>) -> Result<Json<Todo>> {
     let todo = sqlx::query_as("SELECT * FROM todos WHERE id = $1")
-        .bind(*path) 
+        .bind(*path)
         .fetch_one(&state.pool)
         .await
         .map_err(|e| error::ErrorBadRequest(e.to_string()))?;
@@ -68,4 +69,39 @@ struct TodoNew {
 struct Todo {
     pub id: i32,
     pub note: String,
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello, World!"
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+async fn establish_connection() -> Result<Pool<Postgres>, sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://user-saber:YbE57MjodXwH@db.shuttle.rs:5432/db-saber")
+        .await?;
+    Ok(pool)
+}
+
+#[actix_web::test]
+async fn test_server() -> std::io::Result<()> {
+    let pool =establish_connection().await?;
+    let state = web::Data::new(AppState { pool });
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(hello)
+            .service(retrieve)
+            .service(add)
+            .app_data(state)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
